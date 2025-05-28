d738121b416e0af9c56fffa23c9c228a632fdc44
Khouloud Belkhir <koukabelkhir@gmail.com>
2025-05-28 13:23:52 +0200

    Refactor Vite configuration to load environment variables from .env file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
+export default ({ mode }) => {
+  // Load environment variables from .env file
+  const env = loadEnv(mode, process.cwd(), '');
+ 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:  import.meta.env.VITE_BASE_PATH || '/',
+return defineConfig({
+  base:  env.VITE_BASE_PATH || '/',
   plugins: [react()],
-})
+});
+};
