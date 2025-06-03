f0cab36602b688a6f81a1e5826e04513b787a995
Khouloud Belkhir <koukabelkhir@gmail.com>
2025-06-03 12:35:31 +0200

    Update email service and template IDs in Contact component

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_r0ov7th",
+        "template_cvomfz8",
         {
         from_name: form.name,
         to_name: 'Khouloud Belkhir',
@@ -63,7 +63,6 @@ const Contact = () => {
           });
         },
       );
-    console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
   };
 
   return (
